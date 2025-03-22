7d3ac2dbccd1e33efa961695c48c066b3eaf40a6
ArsenySamoylov <samoylov@ispras.ru>
2025-03-22 18:38:34 +0300

    Initial commit

---
+++ rb-tree/src/main.cpp
@@ -0,0 +1,6 @@
+#include <print>
+
+int main() {
+    std::println("splay tree goes br br br");
+    return 0;
+}
---
+++ rb-tree/tests/rb-tree-unit-tests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Basic, HelloWorld) {
+    EXPECT_STRNE("Hello", "world");
+}
